7d16ead0e053b9b7d4d60b4b33b85e11ddb80701
WolfRangerKun <isabofu@hotmail.com>
2021-05-02 16:56:23 -0400

    LO MIo

---
+++ Assets/Scrips/PlaceObject.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceObject : MonoBehaviour
+{
+    public static PlaceObject Create(Vector3 worldPosition, Vector2Int origin, PlacedObjectTypeSO.Dir dir, PlacedObjectTypeSO placedObjectTypeSO)
+    {
+        Transform placedObjectTransform = Instantiate(placedObjectTypeSO.prefab, worldPosition, Quaternion.Euler(0, placedObjectTypeSO.GetRotationAngle(dir), 0));
+
+        PlaceObject placedObject = placedObjectTransform.GetComponent<PlaceObject>();
+        placedObject.Setup(placedObjectTypeSO, origin, dir);
+
+        return placedObject;
+    }
+
+
+
+
+    private PlacedObjectTypeSO placedObjectTypeSO;
+    private Vector2Int origin;
+    private PlacedObjectTypeSO.Dir dir;
+
+    private void Setup(PlacedObjectTypeSO placedObjectTypeSO, Vector2Int origin, PlacedObjectTypeSO.Dir dir)
+    {
+        this.placedObjectTypeSO = placedObjectTypeSO;
+        this.origin = origin;
+        this.dir = dir;
+    }
+
+    public List<Vector2Int> GetGridPositionList()
+    {
+        return placedObjectTypeSO.GetGridPositionList(origin, dir);
+    }
+
+    public void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+
+    public override string ToString()
+    {
+        return placedObjectTypeSO.nameString;
+    }
+
+}
--- Assets/Scrips/TilemapDos.cs
+++ Assets/Scrips/TilemapDos.cs
@@ -5,62 +5,62 @@ using UnityEngine;
 
 public class Tilemap 
 {
-    private GridDos<TilemapObject> grid;
+    //private GridDos<TilemapObject> grid;
 
-    public Tilemap(int width, int heigt, float cellSize, Vector3 originPosition)
-    {
-        grid = new GridDos<TilemapObject>(width, heigt, cellSize, originPosition, (GridDos<TilemapObject> g, int x, int y) => new TilemapObject(g, x, y));
-    }
+    //public Tilemap(int width, int heigt, float cellSize, Vector3 originPosition)
+    //{
+    //    grid = new GridDos<TilemapObject>(width, heigt, cellSize, originPosition, (GridDos<TilemapObject> g, int x, int y) => new TilemapObject(g, x, y));
+    //}
 
-    public void SetTilemapSprite(Vector3 worldPosition, TilemapObject.TilemapSprite tilemapSprite)
-    {
-        TilemapObject tilemapObject =  grid.GetGridObject(worldPosition);
-        if(tilemapObject != null)
-        {
-            tilemapObject.SetTilemapSprite(tilemapSprite);
-        }
-    }
+    //public void SetTilemapSprite(Vector3 worldPosition, TilemapObject.TilemapSprite tilemapSprite)
+    //{
+    //    TilemapObject tilemapObject =  grid.GetGridObject(worldPosition);
+    //    if(tilemapObject != null)
+    //    {
+    //        tilemapObject.SetTilemapSprite(tilemapSprite);
+    //    }
+    //}
 
-    public void SetTilemapVisual(TilemapVisualDos tilemapVisual)
-    {
-        tilemapVisual.SetGrid(grid);
-    }
-
-
-    public class TilemapObject
-    {
-        public enum TilemapSprite
-        {
-            None,
-            Platform
-        }
+    //public void SetTilemapVisual(TilemapVisualDos tilemapVisual)
+    //{
+    //    tilemapVisual.SetGrid(grid);
+    //}
 
 
-        public GridDos<TilemapObject> grid;
-        int x;
-        int y;
-        private TilemapSprite tilemapSprite;
+    //public class TilemapObject
+    //{
+    //    public enum TilemapSprite
+    //    {
+    //        None,
+    //        Platform
+    //    }
+        
 
-        public TilemapObject(GridDos<TilemapObject> grid, int x, int y)
-        {
-            this.grid =grid;
-            this.x = x;
-            this.y = y;
-        }
+    //    public GridDos<TilemapObject> grid;
+    //    int x;
+    //    int y;
+    //    private TilemapSprite tilemapSprite;
 
-        public void SetTilemapSprite(TilemapSprite tilemapSprite)
-        {
-            this.tilemapSprite = tilemapSprite;
-            grid.TriggerGridObjectChanged(x, y);
-        }
+    //    public TilemapObject(GridDos<TilemapObject> grid, int x, int y)
+    //    {
+    //        this.grid =grid;
+    //        this.x = x;
+    //        this.y = y;
+    //    }
 
-        public TilemapSprite GetTilemapSprite()
-        {
-            return tilemapSprite;
-        }
-        public override string ToString()
-        {
-            return tilemapSprite.ToString();
-        }
-    }
+    //    public void SetTilemapSprite(TilemapSprite tilemapSprite)
+    //    {
+    //        this.tilemapSprite = tilemapSprite;
+    //        grid.TriggerGridObjectChanged(x, y);
+    //    }
+
+    //    public TilemapSprite GetTilemapSprite()
+    //    {
+    //        return tilemapSprite;
+    //    }
+    //    public override string ToString()
+    //    {
+    //        return tilemapSprite.ToString();
+    //    }
+    //}
 }
