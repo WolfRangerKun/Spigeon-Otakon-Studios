2848e0b35d1edd4907c1ac5f533e4e1b09ca950f
WolfRangerKun <isabofu@hotmail.com>
2021-05-11 21:46:03 -0400

    Mo

--- Assets/Scrips/ScriptsEnemies/EnemySensorTerrain.cs
+++ Assets/Scrips/ScriptsEnemies/EnemySensorTerrain.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class EnemySensorTerrain : MonoBehaviour
 {
     public Enemy enemy;
+    public AudioSource ruido;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             enemy.following = true;
+            ruido.Play();
         }
 
     }
