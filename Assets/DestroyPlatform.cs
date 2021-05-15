26b1bd25e951bc4e89a76de7df73907d470b5e05
Gaboxz <80337191+Gaboxz@users.noreply.github.com>
2021-05-14 22:23:03 -0400

    xd meme de llorones kliaos

--- Assets/DestroyPlatform.cs
+++ Assets/DestroyPlatform.cs
@@ -9,7 +9,7 @@ public class DestroyPlatform : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 10);
+        Destroy(gameObject, 20);
     }
 
     // Update is called once per frame
